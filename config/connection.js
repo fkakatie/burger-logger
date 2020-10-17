2bee6f8884f4d80de5f60f6b3493e284c409bf4b
fkakatie <sextonskatie@gmail.com>
2020-10-17 14:25:16 -0600

    update connection for heroku deploy

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,22 @@ var mysql = require("mysql");
 require("dotenv").config();
 const my = require('./my.js');
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: my.pass.word,
-  database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  // use jaws db
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  // use local connection
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: my.pass.word,
+    database: "burgers_db"
+  });
+}
+
 
 
 connection.connect(function(err) {
